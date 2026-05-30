747d931a51a4e609be7cb2fe388837ed6d6e82a8
Utkarsh1260 <utkarsh.cyber01@gmail.com>
2026-05-30 17:43:14 +0530

    move function practice files into function folder

--- square.js
+++
@@ -1,8 +0,0 @@
-function square(x){
-   sq=x*x;
-    return sq;
-}
-
-
-var result=square(30);
-console.log(result);
--- sum.js
+++
@@ -1,8 +0,0 @@
-function add(a,b){
-   sum=a+b;
-    return sum;
-}
-
-
-var result=add(10,20);
-console.log(result);
